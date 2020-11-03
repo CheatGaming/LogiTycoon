7f771dd66276bc8de109dcb52bc58842553cac8f
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-03 16:34:19 +0100

    Create Werehouse.js

---
+++ Werehouse.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Werehouse
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.logitycoon.com/eu1/index.php?a=warehouse
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let freight_ids = [];
+
+    function OpenNewTab(freight){
+        window.open('https://www.logitycoon.com/eu1/index.php?a=freight&n=' + freight.id, '_blank');
+    }
+
+    function Refresh() {
+        location.reload(true);
+    }
+
+    function Process(){
+        freight_ids.forEach(OpenNewTab);
+        setTimeout(Refresh, 5000)
+    }
+
+    $('span:contains("Unloaded")')
+        .closest('tr')
+        .each((i,e) => freight_ids.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Unloaded'
+        }));
+    $('span:contains("Arrived")')
+        .closest('tr')
+        .each((i,e) => freight_ids.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Arrived'
+        }));
+    $('span:contains("Loaded")')
+        .closest('tr')
+        .each((i,e) => freight_ids.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Loaded'
+        }));
+    $('span:contains("Accepted")')
+        .closest('tr')
+        .each((i,e) => freight_ids.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Accepted'
+        }));
+
+    setTimeout(Process, 2000)
+})();
