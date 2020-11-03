54edf3180f36f0f98d7298e83a9b692b4f0aca90
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-03 16:32:08 +0100

    Create Employees.js

---
+++ Employees.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         Employees
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.logitycoon.com/eu1/index.php?a=employees
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let employees = [];
+
+    function OpenNewTab(id){
+        window.open('https://www.logitycoon.com/eu1/index.php?a=employees_sleep&e=' + id, '_blank');
+    }
+
+    function AutoRefresh( t ) {
+        setTimeout("location.reload(true);", t);
+    }
+
+    function ParseInnerText(t, percentIndex, actionIndex) {
+        if(!!t){
+            let split = t.split('\t');
+            console.log(split);
+            return {
+                action: split[actionIndex],
+                percent: split[percentIndex]
+            };
+        }
+        return {};
+    }
+
+    function Sleep(employee){
+        if(employee.action !== 'Nothing'){
+            return;
+        }
+        if(employee.percent === '100 %'){
+            return;
+        }
+        OpenNewTab(employee.id);
+    }
+
+    $($('table')[0]).children('tbody')
+        .children('tr')
+        .each((i,e) => {
+        let status = ParseInnerText(e.innerText,4,6);
+        employees.push({
+            id: $(e).children('td').children('a').attr('href').split('=')[2],
+            action: status.action,
+            percent: status.percent
+        });
+    })
+
+    $($('table')[1]).children('tbody')
+        .children('tr')
+        .each((i,e) => {
+        let status = ParseInnerText(e.innerText,4,5);
+        employees.push({
+            id: $(e).children('td').children('a').attr('href').split('=')[2],
+            action: status.action,
+            percent: status.percent
+        });
+    })
+
+    $($('table')[4]).children('tbody')
+        .children('tr')
+        .each((i,e) => {
+        let status = ParseInnerText(e.innerText,3,4);
+        employees.push({
+            id: $(e).children('td').children('a').attr('href').split('=')[2],
+            action: status.action,
+            percent: status.percent
+        });
+    })
+
+    employees.forEach(Sleep);
+
+    AutoRefresh(11000);
+})();
