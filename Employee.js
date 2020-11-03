7d2ba250526df8bd6bd8c702435a3b9636115130
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-03 16:40:49 +0100

    Update Employee.js

--- Employee.js
+++ Employee.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Employee
 // @namespace    https://github.com/TransportScripts/TransportAutomation/
+// @author       TransportScripts
 // @version      0.2
 // @description  try to take over the world!
-// @author       TransportScripts
 // @match        https://www.logitycoon.com/eu1/index.php?a=employees_select&e=*
 // @match        https://www.logitycoon.com/eu1/index.php?a=employees_sleep&e=*
 // @grant        none
