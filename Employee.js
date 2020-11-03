59215903836f7b17563f7b3ccdfdfe4778407d14
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-03 16:31:49 +0100

    Create Employee.js

---
+++ Employee.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Employee
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.logitycoon.com/eu1/index.php?a=employees_select&e=*
+// @match        https://www.logitycoon.com/eu1/index.php?a=employees_sleep&e=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.close();
+})();
