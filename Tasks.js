243982c42e7837e91e3b8d1d3b7f4ad51c3216f0
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-04 12:50:34 +0100

    Update Tasks.js

--- Tasks.js
+++ Tasks.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Tasks
-// @namespace    https://github.com/TransportScripts/TransportAutomation/
+// @namespace    https://github.com/CheatGaming/LogiTycoon/
 // @author       TransportScripts
 // @version      0.1
 // @description  try to take over the world!
 // @match        https://www.logitycoon.com/eu1/index.php?a=tasks
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/TransportScripts/TransportAutomation/main/Tasks.js
+// @downloadURL  https://raw.githubusercontent.com/CheatGaming/LogiTycoon/main/Tasks.js
 // ==/UserScript==
 
 (function() {
