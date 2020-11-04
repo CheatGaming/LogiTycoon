04d77f9ec4173c56fc1185358b3fd342fdf15ed1
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-04 12:50:13 +0100

    Update FuelStation.js

--- FuelStation.js
+++ FuelStation.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Fuel Station
-// @namespace    https://github.com/TransportScripts/TransportAutomation/
+// @namespace    https://github.com/CheatGaming/LogiTycoon/
 // @author       TransportScripts
 // @version      0.1
 // @description  try to take over the world!
 // @match        https://www.logitycoon.com/eu1/index.php?a=fuelstation*
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/TransportScripts/TransportAutomation/main/FuelStation.js
+// @downloadURL  https://raw.githubusercontent.com/CheatGaming/LogiTycoon/main/FuelStation.js
 // ==/UserScript==
 
 (function() {
