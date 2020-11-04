e92e7a5b6ec229d3d8d929268c50c0b90a465452
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-04 12:49:21 +0100

    Update Employee.js

--- Employee.js
+++ Employee.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Employee
-// @namespace    https://github.com/TransportScripts/TransportAutomation/
+// @namespace    https://github.com/CheatGaming/LogiTycoon/
 // @author       TransportScripts
 // @version      0.3
 // @description  try to take over the world!
 // @match        https://www.logitycoon.com/eu1/index.php?a=employees_select&e=*
 // @match        https://www.logitycoon.com/eu1/index.php?a=employees_sleep&e=*
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/TransportScripts/TransportAutomation/main/Employee.js
+// @downloadURL  https://raw.githubusercontent.com/CheatGaming/LogiTycoon/main/Employee.js
 // ==/UserScript==
 
 (function() {
