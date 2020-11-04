0c2aaab7c3ae9cf34097046f01eb9f078318dd68
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-04 13:20:02 +0100

    Update Werehouse.js

--- Werehouse.js
+++ Werehouse.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Werehouse
 // @namespace    https://github.com/CheatGaming/LogiTycoon/
-// @author       TransportScripts
-// @version      0.1
+// @author       CheatGaming
+// @version      0.2
 // @description  try to take over the world!
 // @match        https://www.logitycoon.com/eu1/index.php?a=warehouse
 // @grant        none
@@ -11,10 +11,14 @@
 
 (function() {
     'use strict';
-    let freight_ids = [];
+    let freights = [];
+    let windows = [];
+    
 
     function OpenNewTab(freight){
-        window.open('https://www.logitycoon.com/eu1/index.php?a=freight&n=' + freight.id, '_blank');
+        if(freight.needsAction){
+            windows.push(window.open('https://www.logitycoon.com/eu1/index.php?a=freight&n=' + freight.id, '_blank'));
+        }
     }
 
     function Refresh() {
@@ -22,34 +26,86 @@
     }
 
     function Process(){
-        freight_ids.forEach(OpenNewTab);
-        setTimeout(Refresh, 5000)
+        freights.forEach(OpenNewTab);
+        
+        if(windows.length){
+            setInterval(()=>{
+                if(windows.every(w => w.closed){
+                    Refresh();
+                }
+            }, 500);
+        } else {
+            setTimeout(Refresh, 15000);
+        }
     }
 
-    $('span:contains("Unloaded")')
-        .closest('tr')
-        .each((i,e) => freight_ids.push({
-            id: $(e).attr('onclick').split("=")[3].split("'")[0],
-            state: 'Unloaded'
-        }));
-    $('span:contains("Arrived")')
-        .closest('tr')
-        .each((i,e) => freight_ids.push({
-            id: $(e).attr('onclick').split("=")[3].split("'")[0],
-            state: 'Arrived'
-        }));
-    $('span:contains("Loaded")')
-        .closest('tr')
-        .each((i,e) => freight_ids.push({
-            id: $(e).attr('onclick').split("=")[3].split("'")[0],
-            state: 'Loaded'
-        }));
     $('span:contains("Accepted")')
         .closest('tr')
-        .each((i,e) => freight_ids.push({
+        .each((i,e) => freights.push({
             id: $(e).attr('onclick').split("=")[3].split("'")[0],
-            state: 'Accepted'
+            state: 'Accepted',
+            needsAction: true
+        }));
+    $('span:contains("Loading")')
+        .closest('tr')
+        .each((i,e) => freights.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Loading',
+            needsAction: false
+        }));
+    
+    $('span:contains("Loaded")')
+        .closest('tr')
+        .each((i,e) => freights.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Loaded',
+            needsAction: true
+        }));
+    $('span:contains("Driving")')
+        .closest('tr')
+        .each((i,e) => freights.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Driving',
+            needsAction: false
+        }));
+    
+    $('span:contains("Arrived")')
+        .closest('tr')
+        .each((i,e) => freights.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Arrived',
+            needsAction: true
+        }));
+    $('span:contains("Unloading")')
+        .closest('tr')
+        .each((i,e) => freights.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Unloading',
+            needsAction: false
+        }));
+    
+    $('span:contains("Unloaded")')
+        .closest('tr')
+        .each((i,e) => freights.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Unloaded',
+            needsAction: true
+        }));
+    $('span:contains("Finishing")')
+        .closest('tr')
+        .each((i,e) => freights.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'Unloaded',
+            needsAction: false
+        }));
+    
+    $('span:contains("Out of fuel")')
+        .closest('tr')
+        .each((i,e) => freights.push({
+            id: $(e).attr('onclick').split("=")[3].split("'")[0],
+            state: 'OutOfFuel',
+            needsAction: true
         }));
 
-    setTimeout(Process, 2000)
+    Process();
 })();
