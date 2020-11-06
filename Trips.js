e909fd4aa10f3a4c7054beab8f6658118213c715
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-06 23:11:17 +0100

    Update Trips.js

--- Trips.js
+++ Trips.js
@@ -2,25 +2,49 @@
 // @name         Trips
 // @namespace    https://github.com/CheatGaming/LogiTycoon/
 // @author       TransportScripts
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
-// @match        https://www.logitycoon.com/eu1/index.php?a=trips
+// @match        https://www.logitycoon.com/eu1/index.php?a=trips*
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/CheatGaming/LogiTycoon/main/Trips.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-    try {
-        document.trips.button_1.click();
-        $('#submit-trips').click();
-    }
-    catch (error) {
+    var urlParams = new URLSearchParams(window.location.search);
+
+    function BackToWerehouse(){
+        window.location.href = 'https://www.logitycoon.com/eu1/index.php?a=warehouse';
     }
 
-    function AutoRefresh( t ) {
-        setTimeout("location.reload(true);", t);
+    try{
+        if(urlParams.has('from')){
+            const trips = [];
+            $('tbody').first().find('tr').each((i,e) => {
+                let row = $(e);
+                let split = e.innerText.split(/\t|\n|€/).filter(Boolean);
+                let earnings = parseInt(split[0].replace('.',''));
+                let distance = parseInt(split[3]);
+                trips.push({
+                    from:  split[1].trim(),
+                    to:    split[2].trim(),
+                    earnings: earnings,
+                    distance: distance,
+                    onClick: row.attr('onclick'),
+                    profit: earnings/distance
+                });
+            });
+            trips.sort((a, b) => b.profit - a.profit );
+
+            let from = urlParams.get('from').trim();
+            let trip = trips.find(t => t.from === from);
+            eval(trip.onClick);
+            $('#submit-trips').click();
+        } else {
+            BackToWerehouse();
+        }
+    } catch (e) {
+        BackToWerehouse();
     }
 
-    AutoRefresh(5000);
 })();
