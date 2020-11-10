8c4bb657bb380ae926d1d915da6b743f46a0f25e
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-10 12:01:17 +0100

    Update Freight.js

--- Freight.js
+++ Freight.js
@@ -2,7 +2,7 @@
 // @name         Freight
 // @namespace    https://github.com/CheatGaming/LogiTycoon/
 // @author       TransportScripts
-// @version      0.7
+// @version      0.8
 // @description  try to take over the world!
 // @match        https://www.logitycoon.com/eu1/index.php?a=freight&n=*
 // @grant        none
@@ -18,7 +18,7 @@
         freightautowhemployee();
         freightautotrailer();
         freightstartloading();
-        setTimeout(()=> Utils.GoTo.warehouse(), 50);
+        setTimeout(Utils.GoTo.warehouse, 50);
     }
 
     if(a == '13 %'){ //loading
@@ -29,7 +29,7 @@
         freightautowhemployee();
         freightautotruck();
         freightstartdriving();
-        setTimeout(()=> Utils.GoTo.warehouse(), 50);
+        setTimeout(Utils.GoTo.warehouse, 50);
     }
 
     if(a == '38 %'){ //driving
@@ -38,7 +38,7 @@
 
     if(a == '40 %'){ //Out of Fuel
         freightcontinue();
-        setTimeout(()=> Utils.GoTo.warehouse(), 50);
+        setTimeout(Utils.GoTo.warehouse, 50);
     }
 
     if(a == '50 %'){
@@ -46,24 +46,20 @@
     }
 
     if(a == '63 %'){ //unloading
-        AcceptFreight();
+        let from = $('div.row.static-info:contains(Destination)').find('.value').text().trim();
+        let type = $('div.row.static-info:contains(Type)').find('.value').text().trim();
+
+        Utils.GoTo.trips({from: from, type: type});
     }
 
     if(a == '75 %'){
         freightautowhemployee();
         freightstartfinishing();
-        setTimeout(()=> Utils.GoTo.warehouse(), 50);
+        setTimeout(Utils.GoTo.warehouse, 50);
     }
 
     if(a == '88 %'){
         Utils.GoTo.warehouse();
-    }
-
-    function AcceptFreight(){
-        let from = $('div.row.static-info:contains(Destination)').find('.value').text().trim();
-        let type = $('div.row.static-info:contains(Type)').find('.value').text().trim();
-
-        Utils.GoTo.trips({from: from, type: type});
     }
 
     Utils.Refresh(10000);
