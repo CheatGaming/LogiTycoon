3b05f70a59b2cab45b75bcb157ea189f974f1e1b
Daniel Barwikowski <20668422+dbarwikowski@users.noreply.github.com>
2020-11-10 19:05:12 +0100

    Update Tasks.js

--- Tasks.js
+++ Tasks.js
@@ -2,18 +2,27 @@
 // @name         Tasks
 // @namespace    https://github.com/CheatGaming/LogiTycoon/
 // @author       TransportScripts
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @match        https://www.logitycoon.com/eu1/index.php?a=tasks
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/CheatGaming/LogiTycoon/main/Tasks.js
+// @require      https://raw.githubusercontent.com/CheatGaming/LogiTycoon/main/shared/Utils.js
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    window.open('https://www.logitycoon.com/eu1/index.php?a=garage', '_blank');
-    window.open('https://www.logitycoon.com/eu1/index.php?a=fuelstation&f=10&p=1', '_blank');
-    window.open('https://www.logitycoon.com/eu1/index.php?a=trips', '_blank');
-    window.open('https://www.logitycoon.com/eu1/index.php?a=employees', '_blank');
-    window.open('https://www.logitycoon.com/eu1/index.php?a=warehouse', '_blank');
+//     const actions = [];
+
+    const fuelStation = Utils.Open.fuelStation();
+//     const trips = Utils.Open.trips();
+    const warehouse = Utils.Open.warehouse();
+
+//     window.AddAction = function(action){
+//         actions.push(action);
+//     }
+
+//     fuelStation.AddAction = AddAction;
+//     trips.AddAction = AddAction;
+
+//     setInterval(() => console.log(actions), 5000);
 })();
